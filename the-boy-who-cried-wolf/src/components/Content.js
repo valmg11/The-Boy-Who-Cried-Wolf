65e1c6a3edd5849bf4598b25e4e0cb671767a930
valmg11 <tictactina05@gmail.com>
2026-02-09 13:41:57 -0500

    added title, added more text

--- the-boy-who-cried-wolf/src/components/Content.js
+++ the-boy-who-cried-wolf/src/components/Content.js
@@ -1,7 +1,10 @@
 function Content() {
     return (
-        <p>The Boy Who Cried Wolf is a fable where a shepherd boy tricks other villagers into
-            believing that a wolf attacked his flock.</p>
+        <div>
+            <p>The Boy Who Cried Wolf is a fable where a shepherd boy tricks other villagers into
+                believing that a wolf attacked his flock.</p>
+            <p>Eventually, his lies become true, although the villagers do not believe him this time.</p>
+        </div>
     );
 }
 
--- the-boy-who-cried-wolf/src/components/Footer.js
+++ the-boy-who-cried-wolf/src/components/Footer.js
@@ -1,6 +1,6 @@
 function Footer() {
     return (
-        <footer>The moral of the story is that once you begin to lie, other won'y believe the truth.</footer>
+        <footer>The moral of the story is that once you begin to lie, other won't believe the truth.</footer>
     );
 }
 
