86b2fe521b5e79e289f5caadd35e930358f350b3
valmg11 <tictactina05@gmail.com>
2026-02-11 21:12:15 -0500

    edited comments

--- the-boy-who-cried-wolf/src/App.js
+++ the-boy-who-cried-wolf/src/App.js
@@ -1,18 +1,18 @@
 //Valentina Gandolfo
 import './App.css';
-import Content from './components/Content';
 import Header from './components/Header';
+import Content from './components/Content';
 import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
+      {/* render header, content and footer */}
       <Header />
       <Content />
       <Footer />
     </div>
   );
 }
-
 
 export default App;
